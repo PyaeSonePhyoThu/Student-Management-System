02eac37cd51432cf4ab2f8e2fcc87a8631b1c50f
PyaeSonePhyoThu <pyaes0193@gmail.com>
2024-05-18 17:45:19 +0630

    Add some UI/UX

--- src/App.java
+++ src/App.java
@@ -4,34 +4,67 @@ public class App {
     public static void main(String[] args) throws Exception {
         AdminPanel adminPanel = new AdminPanel();
         Scanner scanner = new Scanner(System.in);
-        String request = "Yes";
-        while(request.equalsIgnoreCase("yes"))
+        int option = 0;
+        while(option != 5)
         {
-            System.out.print("Enter Student ID : ");
-            String studentId = scanner.nextLine();
-            System.out.print("Enter Name : ");
-            String name = scanner.nextLine();
-            System.out.print("Enter Course : ");
-            String course = scanner.nextLine();
-            Student student = new Student(studentId, name, course);
-            adminPanel.createStudent(student);
+            menu();
+            option = scanner.nextInt();
+            scanner.nextLine();
+            switch (option) {
+                case 1:
+                    System.out.print("Enter Student Id : ");
+                    String Id = scanner.nextLine();
+                    System.out.print("Enter Student Name : ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter Course Name : ");
+                    String course = scanner.nextLine();
+                    System.out.println();
 
-            System.out.println("Wanna add? Y/N : ");
-            request = scanner.nextLine();
+                    Student student = new Student(Id, name, course);
+                    adminPanel.createStudent(student);
+                    break;
+                
+                case 2:
+                    System.out.println("Displaying Students : ");
+                    System.out.println(adminPanel.getStudentList());
+                    break;
+
+                case 3: 
+                    System.out.println("Searching Student by Id");
+                    System.out.println("Enter Id to search : ");
+                    String id = scanner.nextLine();
+
+                    System.out.println(adminPanel.searchStudent(id));
+                    break;
+
+                case 4: 
+                    System.out.println("Deleting Student by Id");
+                    System.out.println("Enter Id to delete student record : ");
+                    id = scanner.nextLine();
+                    adminPanel.deleteStudent(id);
+                    break;
+
+                case 5:
+                    System.out.println("This is the project created by InnoLearn Batch-1 Students");
+                    System.out.println("\t\t Thanks");
+
+                default:
+                    break;
+            }
         }
-
-        System.out.println(adminPanel.getStudentList());
-
-        // adminPanel.deleteStudent("1001");
-
-        // System.out.println("Deleting ID : 1001 ------------------");
-
-        // System.out.println(adminPanel.getStudentList());
-
-        // System.out.println("Searching id 1001");
-        System.out.println(adminPanel.searchStudent("1001"));
 
         scanner.close();
 
+    }
+
+    public static void menu()
+    {
+        System.out.println("\t\tWelcome to InnoLearn Student Management System");
+        System.out.println("1. Add Student");
+        System.out.println("2. Display Stuents");
+        System.out.println("3. Display Student by Id");
+        System.out.println("4. Delete Student by Id");
+        System.out.println("5. Exit from Program");
+        System.out.print("Please Enter your selection: ");
     }
 }
